38cd46b80b0480b9e813b208085c6cba60d20620
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-10-02 20:59:49 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [message, setMessage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(r => r.json())
+            .then(data => setMessage(data.message))
+    }, [])
+
+    if (!message) {
+        return <h2>Loading...</h2>
+    }
+
+    return (
+        <div>
+            <p>Here's a lovely fox for you:</p>
+            <img src={message} alt="Random Fox" />
+        </div>
+    );
+}
+
+export default App;
